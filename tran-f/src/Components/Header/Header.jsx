8dc6080a7d45b119f71262f450263983083053fa
Denis Basenko <C:\Users\User\AppData\Roaming\The Bat!>
2020-01-11 01:03:31 +0200

    1 day: Init project create Header component

---
+++ tran-f/src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import cls from './Header.module.css'
+
+const Header = (props) => {
+    return (
+        <header className={cls.wrapper}>
+            
+        </header>
+    )
+}
+export default Header
